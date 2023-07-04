22a70d07d3b0ef4f9f209a7ad5ce2996f3d253c2
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-04 15:30:32 +0900

    white button componentを実装

--- src/components/ui/button/WhiteButton.tsx
+++ src/components/ui/button/WhiteButton.tsx
@@ -1,13 +1,24 @@
 "use client";
+import { ComponentProps } from "react";
 
-const WhiteButton = ({ label }: W_BtnType) => {
+// textSizeを指定することでbutton自体のサイズも調節可能
+const WhiteButton = ({ label, textSize, error, ...props }: W_BtnProps) => {
+  const style = error
+    ? // error=trueの場合、font & border を赤に
+      "bg-white hover:bg-gray-100 py-2 px-4 rounded-full border shadow-md border-red-600 text-red-600 "
+    : "bg-white hover:bg-gray-100 py-2 px-4 rounded-full border shadow-md " +
+      textSize;
   return (
-    <button>{label}</button>
+    <button {...props} className={style}>
+      {label}
+    </button>
   );
 };
 
-type W_BtnType = {
+type W_BtnProps = {
   label: string;
-};
+  textSize?: string;
+  error?: boolean;
+} & ComponentProps<"button">;
 
 export default WhiteButton;
