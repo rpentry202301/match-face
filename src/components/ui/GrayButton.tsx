66eabd9c7262b16f1ab3b8119db43ea39db697e7
mizuki-nakagawa <mizuki.nakagawa@rakus-partners.co.jp>
2023-07-04 17:02:22 +0900

    OrangeButton,GrauButtonコンポーネント作成

---
+++ src/components/ui/GrayButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ComponentProps } from "react";
+
+type ButtonProps = {
+  label: string;
+} & ComponentProps<"button">;
+
+const GrayButton = ({ label, ...props }: GrayBtnProps) => {
+  // console.log(label);
+  const style =
+    " bg-neutral-300 drop-shadow-lg hover:brightness-95 active:drop-shadow-none active:shadow-inner active:mt-0.5 text-black rounded-xl w-40 h-8 text-lg";
+  return (
+    <div>
+      <button className={style} {...props}>
+        {label}
+      </button>
+    </div>
+  );
+};
+
+export default GrayButton;
