28135042627b4b6a060a9981d907e7544db58b91
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-06 00:48:43 +0900

    ヘッダー完成

--- src/components/elements/header/nav/UserIcon.tsx
+++ src/components/elements/header/nav/UserIcon.tsx
@@ -2,11 +2,18 @@ import Image from "next/image";
 
 // UserIcon.tsxは特有の非同期通信が必要なため個別で定義
 // ToDo: ①ユーザーアイコン ②ユーザー名 のfetch
-const UserIcon = async() => {
+const UserIcon = async () => {
   return (
     <div className="flex justify-evenly items-center block border-b-2 border-gray-400 border-opacity-0 p-0.5 w-4/5 m-auto">
-      <div className="relative w-8 h-8 flex justify-center">
-        <Image fill src={"/icon/human_icon.png"} alt="user" sizes="(max-width: 600px) 10vw, (max-width: 1200px) 30vw, 60px" />
+      <div className="rounded-full h-10 w-10 flex items-center justify-center bg-deep-gray">
+        <div className="relative w-8 h-8 flex justify-center">
+          <Image
+            fill
+            src={"/icon/human_icon.png"}
+            alt="user"
+            sizes="(max-width: 600px) 10vw, (max-width: 1200px) 30vw, 60px"
+          />
+        </div>
       </div>
       <p className="text-sm">ユーザー名</p>
     </div>
