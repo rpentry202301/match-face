006ef0546dbbb4cb0585bb4cbc4c2ad970716d5c
YukiNakajima <yuki.nakajima@rakus-partners.co.jp>
2023-07-06 15:42:43 +0900

    モーダル機能を仮作成

--- src/app/(pages)/admin/(other)/tasks/register/page.tsx
+++ src/app/(pages)/admin/(other)/tasks/register/page.tsx
@@ -1,5 +1,13 @@
+import UserSelectModal from "@/components/pages/admin/tasks/register/UserSelectModal";
+
 const TaskRegisterPage = () => {
-  return <div>タスク設定画面</div>;
+  return (
+    <>
+      <div>タスク設定画面</div>
+      {/* テスト用にモーダルコンポーネントを仮設置しています */}
+      <UserSelectModal />
+    </>
+  )
 };
 
 export default TaskRegisterPage;
--- src/components/pages/admin/tasks/register/UserSelectModal.tsx
+++ src/components/pages/admin/tasks/register/UserSelectModal.tsx
@@ -1,7 +1,54 @@
 'use client'
+import { ReactNode, useState } from "react"
+import { createPortal } from "react-dom"
+
+// ラップしたコンポーネントをModal化するコンポーネントを仮作成
+const Modal = ({
+  children,
+  buttonText,
+  canCloseByClickingBackground = true
+}: {
+  children: ReactNode,
+  buttonText: string,
+  canCloseByClickingBackground?: boolean,
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const open = () => setIsOpen(true)
+  const close = () => setIsOpen(false)
+
+  if (!isOpen) {
+    return (
+      <button type="button" onClick={open}>{buttonText}</button>
+    )
+  }
+
+  const elmModal = (
+    <div className="fixed top-0 left-0 z-20 flex items-center justify-center w-full h-full">
+      <div className="relative z-10 box-border w-4/5 max-w-3xl p-10 overflow-y-auto bg-white border-r-4">
+        <button
+          type="button"
+          aria-label="モーダルを閉じる"
+          className="absolute top-1 right-3 p-0 text-4xl font-bold border-none cursor-pointer text-gray-300 bg-transparent"
+          onClick={close}
+        >
+          x
+        </button>
+        {children}
+      </div>
+      {canCloseByClickingBackground && <div
+        className="absolute top-0 left-0 w-full h-full bg-black opacity-30"
+        onClick={close}
+      />}
+    </div>
+  )
+  return createPortal(elmModal, document.body)
+}
 
 const UserSelectModal = () => {
-  return null
+  return <Modal buttonText="追加">
+    test
+  </Modal>
 }
 
 export default UserSelectModal
