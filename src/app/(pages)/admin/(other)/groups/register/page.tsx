f5c1fa2d0bd62ef3fcd499f67af84b1c0f590edc
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-07-06 15:57:38 +0900

    フォームをコンポーネント化

--- src/app/(pages)/admin/(other)/groups/register/page.tsx
+++ src/app/(pages)/admin/(other)/groups/register/page.tsx
@@ -2,28 +2,11 @@ import OrangeButton from "@/components/ui/button/OrangeButton";
 import WhiteButton from "@/components/ui/button/WhiteButton";
 import Link from "next/link";
 import Input from "@/components/ui/Input";
+import RegisterForm from "@/components/pages/admin/groups/register/RegisterForm";
 
 
 const GroupRegisterPage = () => {
-  return (
-    <>
-      <div>
-        <form>
-          <label htmlFor="group_name">▶グループ名を設定する</label>
-          <Input id="group_name"/> 
-          <label htmlFor="user">▶ユーザーを選択する</label>
-          <WhiteButton label="追加"/>
-          <Input id="user"/>
-          <label htmlFor="group_description">▶備考</label>
-          <Input id="group_description"/>
-        </form>
-      </div>
-      <br />
-      <Link href={"/admin/groups"}>
-        <OrangeButton label="グループを設定する" />
-      </Link>
-    </>
-  );
+  return <RegisterForm/>
 };
 
 export default GroupRegisterPage;
