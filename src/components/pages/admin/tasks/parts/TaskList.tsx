365f4eb4a35bb5f1b7ba22f9c102e4133b7ceb3a
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-06 17:20:58 +0900

    タスク一覧画面の実装

--- src/components/pages/admin/tasks/parts/TaskList.tsx
+++ src/components/pages/admin/tasks/parts/TaskList.tsx
@@ -1,7 +1,38 @@
+import { tasks } from "@/const/tasks";
+
+// Todo: タスクリストを非同期通信で取得
 const TaskList = () => {
-    return (
-        <div>タスク一覧</div>
-    );
+  return (
+    <div>
+      <table className="border-collapse border-2 border-deep-gray text-sm">
+        <thead className="bg-light-gray">
+          <tr>
+            <th className="border-2 border-deep-gray p-2 text-center w-72">案件名</th>
+            <th className="border-2 border-deep-gray p-2 text-center w-16">質問数</th>
+            <th className="border-2 border-deep-gray p-2 text-center w-16">職種</th>
+            <th className="border-2 border-deep-gray p-2 text-center w-72">回答ユーザー</th>
+            <th className="border-2 border-deep-gray p-2 text-center w-28">期日</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td className="border-2 border-deep-gray p-2 text-center">{task.project_name}</td>
+              <td className="border-2 border-deep-gray p-2 text-center">{task.question.length}</td>
+              <td className="border-2 border-deep-gray p-2 text-center">{task.department}</td>
+              <td className="border-2 border-deep-gray p-2 text-center">
+                {/* 回答ユーザーは3名まで表示。それ以降は"..."で省略 */}
+                {task.users.length > 3
+                  ? task.users.slice(0, 3).join("、") + "..."
+                  : task.users.join("、")}
+              </td>
+              <td className="border-2 border-deep-gray p-2 text-center">{task.answer_deadline}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export default TaskList
+export default TaskList;
--- src/const/tasks.ts
+++ src/const/tasks.ts
@@ -24,3 +24,54 @@ export const departments = [
     name: "QA",
   },
 ];
+
+export const tasks = [
+  {
+    id: 1,
+    project_name: "hoge",
+    department: "Java",
+    question: ["hoge", "foo", "bar"],
+    users: ["佐藤太郎", "鈴木次郎", "伊藤三郎"],
+    answer_deadline: "2100-01-01",
+  },
+  {
+    id: 2,
+    project_name: "foo",
+    department: "PHP",
+    question: ["hoge", "foo", "bar"],
+    users: ["佐藤太郎", "鈴木次郎", "伊藤三郎", "田中四郎"],
+    answer_deadline: "2100-01-01",
+  },
+  {
+    id: 3,
+    project_name: "bar",
+    department: "FR",
+    question: ["hoge", "foo", "bar"],
+    users: ["佐藤太郎", "鈴木次郎", "伊藤三郎"],
+    answer_deadline: "2100-01-01",
+  },
+  {
+    id: 4,
+    project_name: "hogehoge",
+    department: "CL",
+    question: ["hoge", "foo", "bar"],
+    users: ["佐藤太郎", "鈴木次郎", "伊藤三郎"],
+    answer_deadline: "2100-01-01",
+  },
+  {
+    id: 5,
+    project_name: "foofoo",
+    department: "ML",
+    question: ["hoge", "foo", "bar"],
+    users: ["佐藤太郎", "鈴木次郎", "伊藤三郎", "田中四郎"],
+    answer_deadline: "2100-01-01",
+  },
+  {
+    id: 6,
+    project_name: "barbar",
+    department: "QA",
+    question: ["hoge", "foo", "bar"],
+    users: ["佐藤太郎", "鈴木次郎", "伊藤三郎"],
+    answer_deadline: "2100-01-01",
+  },
+];
