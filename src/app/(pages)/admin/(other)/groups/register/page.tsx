5e768dd32682542206d5d81d406948e230602251
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-07-06 15:52:56 +0900

    グーグル設定画面入力フォーム配置

--- src/app/(pages)/admin/(other)/groups/register/page.tsx
+++ src/app/(pages)/admin/(other)/groups/register/page.tsx
@@ -1,10 +1,24 @@
 import OrangeButton from "@/components/ui/button/OrangeButton";
+import WhiteButton from "@/components/ui/button/WhiteButton";
 import Link from "next/link";
+import Input from "@/components/ui/Input";
+
 
 const GroupRegisterPage = () => {
   return (
     <>
-      <div>グループ設定画面</div>
+      <div>
+        <form>
+          <label htmlFor="group_name">▶グループ名を設定する</label>
+          <Input id="group_name"/> 
+          <label htmlFor="user">▶ユーザーを選択する</label>
+          <WhiteButton label="追加"/>
+          <Input id="user"/>
+          <label htmlFor="group_description">▶備考</label>
+          <Input id="group_description"/>
+        </form>
+      </div>
+      <br />
       <Link href={"/admin/groups"}>
         <OrangeButton label="グループを設定する" />
       </Link>
