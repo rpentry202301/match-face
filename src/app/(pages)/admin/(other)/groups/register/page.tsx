9ceb198490f988b815b6f479373c86a512ff8577
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-07-06 16:26:11 +0900

    レイアウト調整

--- src/app/(pages)/admin/(other)/groups/register/page.tsx
+++ src/app/(pages)/admin/(other)/groups/register/page.tsx
@@ -1,7 +1,3 @@
-import OrangeButton from "@/components/ui/button/OrangeButton";
-import WhiteButton from "@/components/ui/button/WhiteButton";
-import Link from "next/link";
-import Input from "@/components/ui/Input";
 import RegisterForm from "@/components/pages/admin/groups/register/RegisterForm";
 
 
--- src/components/pages/admin/groups/register/RegisterForm.tsx
+++ src/components/pages/admin/groups/register/RegisterForm.tsx
@@ -10,15 +10,18 @@ const RegisterForm = () => {
         <form>
           <label htmlFor="group_name">▶グループ名を設定する</label>
           <Input id="group_name" />
+          <br />
           <label htmlFor="user">▶ユーザーを選択する</label>
-          <WhiteButton label="追加" />
+          <span>&nbsp;</span>
+          <WhiteButton label="追加" className=" px-3 py-1 text-xs" />
           <Input id="user" />
+          <br />
           <label htmlFor="group_description">▶備考</label>
           <Input id="group_description" />
         </form>
         <br />
         <Link href={"/admin/groups"}>
-          <OrangeButton label="グループを設定する" />
+          <OrangeButton label="グループを設定する" className="py-19 text-xs" />
           {/* のちのちonclickでpostします */}
         </Link>
       </div>
