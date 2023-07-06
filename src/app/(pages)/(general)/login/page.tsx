d9f94284c199f697f0ff7a4ae1de0f47955c88d3
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-07-06 13:37:39 +0900

    ユーザーログイン画面、管理者ログイン画面完成

--- src/app/(pages)/(general)/login/page.tsx
+++ src/app/(pages)/(general)/login/page.tsx
@@ -5,24 +5,28 @@ import Link from 'next/link';
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className=" text-orange text-2xl font-bold">Match Face</h1>
+      <h1 className=" text-orange text-2xl font-bold m-6">Match Face</h1>
 
       <div className="flex flex-col">
-        <label htmlFor="userId" className="">
-          ユーザーID
-        </label>
-        <Input id="userId" className="w-72" />
-        <label htmlFor="password" className="">
-          パスワード
-        </label>
-        <Input id="password" className="w-72" />
+        <form className="mt-2 mb-2">
+          <label htmlFor="userId" className="">
+            ユーザーID
+          </label>
+          <Input id="userId" className="w-72" />
+        </form>
+        <form>
+          <label htmlFor="password" className="">
+            パスワード
+          </label>
+          <Input id="password" className="w-72" />
+        </form>
       </div>
 
-      <OrangeButton label="ログイン" className=" m-4" />
+      <OrangeButton label="ログイン" className="m-4" />
       <Link href="/remind" className=" text-blue">
         パスワードを忘れた
       </Link>
-      <Link href="/admin/login" className="text-blue">
+      <Link href="/admin/login" className="text-blue pt-16">
         管理者ログイン
       </Link>
     </div>
