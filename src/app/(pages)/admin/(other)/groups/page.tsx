a3fcce7ce25c2e89363714107068497ff1d7b2c1
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-07-06 16:16:24 +0900

    配置を中央寄せ

--- src/app/(pages)/admin/(other)/groups/page.tsx
+++ src/app/(pages)/admin/(other)/groups/page.tsx
@@ -1,17 +1,7 @@
-import OrangeButton from "@/components/ui/button/OrangeButton";
-import Link from "next/link";
 import GroupTable from "@/components/pages/admin/groups/GroupTable";
 
 const GroupsPage = () => {
-  return (
-    <>
-      <GroupTable/>
-      <br />
-      <Link href={"/admin/groups/register"}>
-        <OrangeButton label="新規グループ作成" />
-      </Link>
-    </>
-  );
+  return <GroupTable />;
 };
 
 export default GroupsPage;
--- src/components/pages/admin/groups/register/RegisterForm.tsx
+++ src/components/pages/admin/groups/register/RegisterForm.tsx
@@ -3,26 +3,25 @@ import WhiteButton from "@/components/ui/button/WhiteButton";
 import Link from "next/link";
 import Input from "@/components/ui/Input";
 
-
 const RegisterForm = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center justify-center h-screen">
         <form>
           <label htmlFor="group_name">▶グループ名を設定する</label>
-          <Input id="group_name"/> 
+          <Input id="group_name" />
           <label htmlFor="user">▶ユーザーを選択する</label>
-          <WhiteButton label="追加"/>
-          <Input id="user"/>
+          <WhiteButton label="追加" />
+          <Input id="user" />
           <label htmlFor="group_description">▶備考</label>
-          <Input id="group_description"/>
+          <Input id="group_description" />
         </form>
+        <br />
+        <Link href={"/admin/groups"}>
+          <OrangeButton label="グループを設定する" />
+          {/* のちのちonclickでpostします */}
+        </Link>
       </div>
-      <br />
-      <Link href={"/admin/groups"}>
-        <OrangeButton label="グループを設定する" />
-        {/* のちのちonclickでpostします */}
-      </Link>
     </>
   );
 };
