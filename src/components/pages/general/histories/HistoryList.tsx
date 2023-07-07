bd4ad537e444218520ade9c240a8727d1d86e8c6
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-07-07 17:12:37 +0900

    途中

--- src/components/pages/general/histories/HistoryList.tsx
+++ src/components/pages/general/histories/HistoryList.tsx
@@ -2,6 +2,7 @@ import GrayButton from "@/components/ui/button/GrayButton";
 import OrangeButton from "@/components/ui/button/OrangeButton";
 import { data } from "@/const/histories";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type ProjectData = {
   id: string;
@@ -12,13 +13,11 @@ type ProjectData = {
 }[];
 
 const HistoryList = () => {
+  const router = useRouter();
   // const response = await fetch('http://localhost:3000/api');
   // if (!response.ok) throw new Error('Failed to fetch data');
   // const projectData: ProjectData = await response.json();
   const projectData: ProjectData = data;
-  const toTesting = () => {
-
-  }
   return (
     <div>
       <table className="table-auto border border-collapse my-20">
@@ -48,11 +47,18 @@ const HistoryList = () => {
               <td className="border text-center">{project.project_name}</td>
               <td className="border text-center">{project.project_detail}</td>
               <td className="border text-center px-4">
-                  <GrayButton
-                    label={"詳細"}
-                    className="w-15 rounded py-2 px-4 text-sm"
-                    onClick={toTesting}project.id
-                  />
+                {/* Linkでもできるのでは？ */}
+                <GrayButton
+                  label={"詳細"}
+                  className="w-15 rounded py-2 px-4 text-sm"
+                  value={project.id}
+                  onClick={() => {
+                    router.push({
+                      pathname: "/result/",
+                      query: { id: project.id },
+                    });
+                  }}
+                />
               </td>
             </tr>
           ))}
