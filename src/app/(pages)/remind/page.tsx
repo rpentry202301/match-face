31b45b5e50b27d8aeb6eb5eeb0cfb199d2a11c3b
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-07-07 10:18:40 +0900

    パスワード再設定画面①の外装完了

--- src/app/(pages)/remind/page.tsx
+++ src/app/(pages)/remind/page.tsx
@@ -1,14 +1,35 @@
+'use client';
 import Input from '@/components/ui/Input';
+import OrangeButton from '@/components/ui/button/OrangeButton';
+import Link from 'next/link';
 
 const RemindPage = () => {
+  const sentRemindEmail = () => {
+    console.log('リマインドメールが送信されました');
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className=" text-orange text-2xl font-bold m-6">Match Face</h1>
-        <p>パスワード再設定用のURLを指定のメールアドレスに送信します。</p>
-        <div className="flex flex-col">
+        <span className="mb-6">
+          パスワード再設定用のURLを指定のメールアドレスに送信します。
+        </span>
+
+        <form className="flex flex-col">
           <label htmlFor="email">メールアドレス</label>
-          <Input id="email" />
+          <Input id="email" className=" w-72" />
+        </form>
+
+        <div className="flex flex-col items-center">
+          <OrangeButton
+            label="再設定用メール送信"
+            className=" mt-12 w-52"
+            onClick={sentRemindEmail}
+          />
+          <Link href="/login" className="text-blue mt-3">
+            一般ログインへ
+          </Link>
         </div>
       </div>
     </>
