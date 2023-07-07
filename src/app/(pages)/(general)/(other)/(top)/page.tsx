f300fb7d4257ae11d610e93c42ae5cfbd52342df
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-07-07 16:25:35 +0900

    Menuコンポーネントの設定

--- src/app/(pages)/(general)/(other)/(top)/page.tsx
+++ src/app/(pages)/(general)/(other)/(top)/page.tsx
@@ -1,7 +1,24 @@
-const Home = () => {
+import Menu from '@/components/ui/Menu';
+
+const Home = async () => {
   return (
     <main>
       <div>トップページ</div>
+
+      <Menu
+        url="/questions"
+        title="回答する"
+        description="質問に答えて顔合わせの準備をします"
+        imgUrl="/icon/human_icon.png"
+        imgAlt="回答一覧画面アイコン"
+      />
+      <Menu
+        url="/histories"
+        title="回答履歴"
+        description="自身の回答をここから確認してください"
+        imgUrl="/icon/human_icon.png"
+        imgAlt="回答履歴画面アイコン"
+      />
     </main>
   );
 };
