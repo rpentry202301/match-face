5e19e7e21399621f79497a5e9a9033ab8662fd7b
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-07 14:41:42 +0900

    タスク設定画面の実装

--- src/components/pages/admin/tasks/register/parts/SelectDeadline.tsx
+++ src/components/pages/admin/tasks/register/parts/SelectDeadline.tsx
@@ -1,12 +1,22 @@
+'use client';
+import SelectBox from "@/components/ui/selectbox/SelectBox";
+
 const SelectDeadline = () => {
   return (
     <div>
       <div className="flex items-center">
-        <p>締め切り日時を設定する</p>
-        <p>　この辺にセレクトボックス</p>
+        <div>
+          <p>締め切り日時を設定する</p>
+        </div>
+        <div>
+          <SelectBox className="w-20 h-6 text-sm" value={hoge}/>
+        </div>
       </div>
     </div>
   );
 };
 
 export default SelectDeadline;
+
+// 削除予定
+const hoge = Array(20).fill("hoge");
