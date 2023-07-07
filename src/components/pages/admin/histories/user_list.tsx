1f962e4fc2e548912687e95b779c58f5be5ef3e2
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2023-07-07 09:11:21 +0900

    gray_button

--- src/components/pages/admin/histories/user_list.tsx
+++ src/components/pages/admin/histories/user_list.tsx
@@ -1,4 +1,5 @@
 import { answered_user } from "@/const/admin_histories"
+import GrayButton from "@/components/ui/button/GrayButton"
 export function HistoriesUserListHead(){
     return(
         <>            
@@ -17,7 +18,7 @@ export function HistoriesUserListBody({id}:{id:number}){
             {project_answered_user.map((user)=>(
                 <tr key={user.id}> 
                 <td></td>
-                <td className="border border-slate-deep-gray" id="detail"><button type="button">詳細</button></td>
+                <td className="border border-slate-deep-gray" id="detail"><GrayButton label="詳細" className="bg-neutral-300 drop-shadow-lg hover:brightness-95 active:drop-shadow-none active:shadow-inner active:mt-0.5 text-black rounded-xl w-20 h-[20px] text-sm"/></td>
                 <td className="border border-slate-deep-gray" id="answer_status">{user.answer_status?"回答済み":"未回答"}</td>
                 <td className="border border-slate-deep-gray" id="user_name">{user.user_name}</td>
                 </tr>
