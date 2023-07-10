6187f0e6c51648c7cf918160c038ac40d0977a32
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-07-10 10:55:42 +0900

    topページの外装完了

--- src/app/(pages)/(general)/(other)/(top)/page.tsx
+++ src/app/(pages)/(general)/(other)/(top)/page.tsx
@@ -4,18 +4,25 @@ import Notification from '@/components/pages/general/top/Notification';
 const Home = async () => {
   return (
     <main>
-      <div className="flex flex-col items-center">
-        <Notification title="進捗状況" className="" />
+      <div className="flex flex-col items-center mt-8 mb-20">
+        <Notification
+          title="進捗状況"
+          completedTasksNumbers={12}
+          incompletedTasksNumbers={10}
+          deadlineDay="12月14日"
+          deadlineNumbers={10}
+          className=""
+        />
       </div>
 
-      <div className="flex flex-wrap justify-center mt-20">
+      <div className="flex flex-wrap justify-center">
         <Menu
           url="/questions"
           title="回答する"
           description="質問に答えて顔合わせの準備をします。"
           imgUrl="/icon/document_icon.png"
           imgAlt="回答一覧画面アイコン"
-          className=""
+          className=" mr-20"
         />
         <Menu
           url="/histories"
@@ -23,6 +30,7 @@ const Home = async () => {
           description="自身の回答をここから確認してください。"
           imgUrl="/icon/history_icon.png"
           imgAlt="回答履歴画面アイコン"
+          className=" ml-20"
         />
       </div>
     </main>
--- src/components/pages/general/top/Notification.tsx
+++ src/components/pages/general/top/Notification.tsx
@@ -2,17 +2,23 @@ import { twMerge } from 'tailwind-merge';
 
 type MenuProps = {
   title: string;
-  // description: string;
+  completedTasksNumbers: number;
+  incompletedTasksNumbers: number;
+  deadlineDay: string;
+  deadlineNumbers: number;
   className: string;
 };
 
 export default function Notification({
   title,
-  // description,
+  completedTasksNumbers,
+  incompletedTasksNumbers,
+  deadlineDay,
+  deadlineNumbers,
   className,
 }: MenuProps) {
   //buttonのclassNameは親コンポーネントでclassNameを指定して上書き可能
-  const mergedClassName = twMerge('p-5 w-1/2 h-[360px] shadow-lg', className);
+  const mergedClassName = twMerge('p-5 w-1/2 h-auto shadow-lg', className);
   return (
     <>
       <div className={mergedClassName}>
@@ -20,19 +26,31 @@ export default function Notification({
           {title}
         </h1>
 
-        <div className="pt-4">
+        <div className="">
           {/* answer_statusがtrueの件数? */}
-          <h2 className="">【対応済】15件</h2>
+          <h2 className="">【対応済】{completedTasksNumbers}件</h2>
           {/* answer_statusがfalseの件数? */}
-          <h2 className="pt-4">【未対応】30件</h2>
-          {/* answer_deadline(6行まで) */}
+          <h2 className="pt-4">【未対応】{incompletedTasksNumbers}件</h2>
+          {/* データ構造がはっきりしてから編集要*/}
           <ul className=" pl-4">
-            <li className="pt-2">7月20日までに対応必須の項目が3件あります</li>
-            <li className="pt-2">8月12日までに対応必須の項目が3件あります</li>
-            <li className="pt-2">8月12日までに対応必須の項目が3件あります</li>
-            <li className="pt-2">8月12日までに対応必須の項目が3件あります</li>
-            <li className="pt-2">8月12日までに対応必須の項目が3件あります</li>
-            <li className="pt-2">8月12日までに対応必須の項目が3件あります</li>
+            <li className="pt-2">
+              {deadlineDay}までに対応必須の項目が{deadlineNumbers}件あります
+            </li>
+            <li className="pt-2">
+              {deadlineDay}までに対応必須の項目が{deadlineNumbers}件あります
+            </li>
+            <li className="pt-2">
+              {deadlineDay}までに対応必須の項目が{deadlineNumbers}件あります
+            </li>
+            <li className="pt-2">
+              {deadlineDay}までに対応必須の項目が{deadlineNumbers}件あります
+            </li>
+            <li className="pt-2">
+              {deadlineDay}までに対応必須の項目が{deadlineNumbers}件あります
+            </li>
+            <li className="pt-2">
+              {deadlineDay}までに対応必須の項目が{deadlineNumbers}件あります
+            </li>
           </ul>
         </div>
       </div>
