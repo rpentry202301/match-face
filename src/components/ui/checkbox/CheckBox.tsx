941a5a5f0ca317ac0869a4e732ad6e8e1fe8b9fb
YukiNakajima <yuki.nakajima@rakus-partners.co.jp>
2023-07-10 09:44:40 +0900

    CheckBoxにtwMergeを使用

--- src/components/ui/checkbox/CheckBox.tsx
+++ src/components/ui/checkbox/CheckBox.tsx
@@ -1,9 +1,14 @@
 'use client'
 import { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
 
 const CheckBox = ({ ...props }: ComponentProps<"input">) => {
+  const style = twMerge(
+    "w-4 h-4 accent-orange",
+    props.className
+  )
   return (
-    <input type="checkbox" {...props} className="w-4 h-4 accent-orange" />
+    <input type="checkbox" {...props} className={style} />
   )
 }
 
