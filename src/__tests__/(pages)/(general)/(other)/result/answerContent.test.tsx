0d519f27678d279aaf622cf6fd547a06601e686e
yukatakahashi <yuka.takahashi@rakus-partners.co.jp>
2023-07-11 17:02:43 +0900

    answerContentのスナップショットテストを追加しました

---
+++ src/__tests__/(pages)/(general)/(other)/result/answerContent.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { AnswerContent } from "@/app/(pages)/(general)/(other)/result/[id]/answerContent";
+import { useRouter } from "next/navigation";
+import { AppRouterContextProviderMock } from "./app-router-context-provider-mock";
+
+describe("AnswerContentコンポーネントのテスト", () => {
+  const testData = {
+    user_id: "user1",
+    project_id: "1",
+  };
+
+  test("スナップショットテスト", () => {
+    const push = jest.fn();
+
+    const contents = render(
+      <AppRouterContextProviderMock router={{ push }}>
+        <AnswerContent
+          user_id={testData.user_id}
+          project_id={testData.project_id}
+        />
+      </AppRouterContextProviderMock>
+    );
+    expect(contents).toMatchSnapshot();
+  });
+});
