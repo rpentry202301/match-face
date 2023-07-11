95d8948df62144ec8e6e6c346494c9b97912cb73
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-11 17:07:27 +0900

    <select>にdata-testidを付与

--- src/__tests__/components/admin/tasks/register/parts/SelectDeadline.test.tsx
+++ src/__tests__/components/admin/tasks/register/parts/SelectDeadline.test.tsx
@@ -1,5 +1,6 @@
 import SelectDeadline from "@/components/pages/admin/tasks/register/parts/SelectDeadline";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("SelectDeadline.tsx", () => {
   beforeEach(() => {
@@ -9,8 +10,26 @@ describe("SelectDeadline.tsx", () => {
     jest.clearAllMocks();
   });
 
-  it("レンダリング時", () => {
-    const {container} = render(<SelectDeadline />);
-    expect(container).toMatchSnapshot();
+  describe("スナップショットテスト", () => {
+    it("レンダリング時", () => {
+      const { container } = render(<SelectDeadline />);
+      expect(container).toMatchSnapshot();
+    });
+  });
+  describe("入力テスト", () => {
+    const user = userEvent.setup();
+    beforeEach(() => {
+      render(<SelectDeadline />);
+    });
+    it("「年」セレクトボックス選択", async () => {
+      const beforeSelectYear = screen.getByRole("option", {
+        name: "2023",
+      }) as HTMLOptionElement;
+      expect(beforeSelectYear.selected).toBe(true);
+      await user.selectOptions(screen.getByTestId("year"), "2024");
+      const selectYear = screen.getByRole("option", {name: "2024"}) as HTMLOptionElement;
+      expect(selectYear.selected).toBe(true);
+      expect(beforeSelectYear.selected).toBe(false)
+    });
   });
 });
