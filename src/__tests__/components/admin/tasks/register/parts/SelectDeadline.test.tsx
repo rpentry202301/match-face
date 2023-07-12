044ffe250dc88501588780ef599c3e820921e144
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-12 11:25:26 +0900

    SelectDeadline.tsxのセレクトボックステスト

--- src/__tests__/components/admin/tasks/register/parts/SelectDeadline.test.tsx
+++ src/__tests__/components/admin/tasks/register/parts/SelectDeadline.test.tsx
@@ -16,56 +16,57 @@ describe("SelectDeadline.tsx", () => {
       expect(container).toMatchSnapshot();
     });
   });
+  
   describe("入力テスト", () => {
     const user = userEvent.setup();
     beforeEach(() => {
       render(<SelectDeadline />);
     });
     it("「年」セレクトボックス選択", async () => {
+      // 現在選択されている値をテスト
+      const thisYear = new Date().getFullYear();
       const beforeSelect = screen.getByRole("option", {
-        name: "2023",
+        name: `${thisYear}`,
       }) as HTMLOptionElement;
       expect(beforeSelect.selected).toBe(true);
-      await user.selectOptions(screen.getByTestId("year"), "2024");
+      // 異なる年を選択してテスト
+      await user.selectOptions(screen.getByTestId("year"), `${thisYear + 1}`);
       const afterSelect = screen.getByRole("option", {
-        name: "2024",
+        name: `${thisYear + 1}`,
       }) as HTMLOptionElement;
       expect(afterSelect.selected).toBe(true);
+      // 最初に選択されていた値との差分をテスト
       expect(beforeSelect.selected).toBe(false);
     });
     it("「月」セレクトボックス選択", async () => {
-      const beforeSelect = screen.getByRole("option", {
-        name: "1",
-      }) as HTMLOptionElement;
+      // デフォルトは今月
+      const thisMonth = new Date().getMonth() + 1;
+      const beforeSelect = screen.getByTestId(`month:${thisMonth}`) as HTMLOptionElement;
       expect(beforeSelect.selected).toBe(true);
-      await user.selectOptions(screen.getByTestId("month"), "12");
-      const afterSelect = screen.getByRole("option", {
-        name: "12",
-      }) as HTMLOptionElement;
+      // 翌月を選択
+      const otherMonth = thisMonth + 1 >= 13 ? 1 : thisMonth + 1;
+      await user.selectOptions(screen.getByTestId("month"), `${otherMonth}`);
+      const afterSelect = screen.getByTestId(`month:${otherMonth}`) as HTMLOptionElement;
       expect(afterSelect.selected).toBe(true);
       expect(beforeSelect.selected).toBe(false);
     });
     it("「日」セレクトボックス選択", async () => {
-      const beforeSelect = screen.getByRole("option", {
-        name: "1",
-      }) as HTMLOptionElement;
+      // デフォルトは1日
+      const beforeSelect = screen.getByTestId("day:1") as HTMLOptionElement;
       expect(beforeSelect.selected).toBe(true);
-      await user.selectOptions(screen.getByTestId("day"), "25");
-      const afterSelect = screen.getByRole("option", {
-        name: "25",
-      }) as HTMLOptionElement;
+      // 11日を選択
+      await user.selectOptions(screen.getByTestId("day"), "11");
+      const afterSelect = screen.getByTestId("day:11") as HTMLOptionElement;
       expect(afterSelect.selected).toBe(true);
       expect(beforeSelect.selected).toBe(false);
     });
     it("「時間」セレクトボックス選択", async () => {
-      const beforeSelect = screen.getByRole("option", {
-        name: "0",
-      }) as HTMLOptionElement;
+      // デフォルトは18時
+      const beforeSelect = screen.getByTestId("time:18") as HTMLOptionElement;
       expect(beforeSelect.selected).toBe(true);
-      await user.selectOptions(screen.getByTestId("time"), "23");
-      const afterSelect = screen.getByRole("option", {
-        name: "23",
-      }) as HTMLOptionElement;
+      // 15時を選択
+      await user.selectOptions(screen.getByTestId("time"), "15");
+      const afterSelect = screen.getByTestId("time:15") as HTMLOptionElement;
       expect(afterSelect.selected).toBe(true);
       expect(beforeSelect.selected).toBe(false);
     });
