15a1b1cf5cb745f6580977ac6975bb5e2b942a46
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-12 16:36:56 +0900

    一般ユーザー画面のタイトルテンプレートを設定 ログイン画面タイトル追加

---
+++ src/app/(pages)/(general)/login/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "ログイン",
+  description: "一般ユーザーログインページ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
