581b9d5aa2929e718e9c18e71db9a76b28cdc24a
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-12 14:05:15 +0900

    タスク一覧/タスク設定画面のテスト完了

--- src/__tests__/components/admin/tasks/index/TasksIndex.test.tsx
+++ src/__tests__/components/admin/tasks/index/TasksIndex.test.tsx
@@ -1,5 +1,6 @@
 import TasksIndex from "@/components/pages/admin/tasks/index/TasksIndex";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("TaskIndex.tsx", () => {
   beforeEach(() => {
@@ -8,8 +9,15 @@ describe("TaskIndex.tsx", () => {
   afterAll(() => {
     jest.clearAllMocks();
   });
+
   it("レンダリング時", () => {
     const { container } = render(<TasksIndex />);
     expect(container).toMatchSnapshot();
+  });
+  it("新規タスク作成ボタンリンク", () => {
+    render(<TasksIndex />);
+    const LinkButton = screen.getByTestId("link_task_register");
+    // console.log("LinkButton", LinkButton);
+    expect(LinkButton).toHaveAttribute("href", "/admin/tasks/register");
   });
 });
