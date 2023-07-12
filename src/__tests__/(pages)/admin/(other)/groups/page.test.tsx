7030b41348ee266de72a8dcade13122e77c1d405
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-07-12 12:04:22 +0900

    テストファイルの作成および関連IDなどの付与

--- src/__tests__/(pages)/admin/(other)/groups/page.test.tsx
+++ src/__tests__/(pages)/admin/(other)/groups/page.test.tsx
@@ -1,4 +1,4 @@
-import { render,screen } from '@testing-library/react'
+import {render,screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import GroupsPage from '@/app/(pages)/admin/(other)/groups/page'
 import React from 'react'
@@ -13,6 +13,21 @@ describe('管理者/グループ一覧画面のテスト',()=>{
         it('レンダリング時',async()=>{
             const view = render(<GroupsPage/>)
             expect(view.container).toMatchSnapshot()
+        })
+    })
+    describe('モーダル表示テスト',()=>{
+        it('グループ名押下で詳細表示',async()=>{
+            const groupName = screen.getByTestId('group_1')
+            await user.click(groupName)
+            const modalTabel = screen.getByTestId('modalTable')
+            expect(modalTabel).toBeInTheDocument
+        })
+    })
+
+    describe('新規グループ設定ページへの遷移',()=>{
+        it('ボタン押下でページ遷移',async()=>{
+            const registerButton =screen.getByTestId('register')
+            expect(registerButton).toHaveAttribute("href","/admin/groups/register")
         })
     })
 })
