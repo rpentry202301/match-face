e756ead1b33dc97a4fc8a3c5757a694d3f236897
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-13 17:47:14 +0900

    テストコードをちょこっと改変

--- src/__tests__/components/elements/header/nav/template/MenuTemp.test.tsx
+++ src/__tests__/components/elements/header/nav/template/MenuTemp.test.tsx
@@ -1,7 +1,7 @@
 import MenuTemp from "@/components/elements/header/nav/template/MenuTemp";
 import { render } from "@testing-library/react";
 
-describe("elements/header/nav/template/MenuTemp.tsx", () => {
+describe("MenuTemp.tsx", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
--- src/__tests__/components/ui/logo/MatchFaceLogo.test.tsx
+++ src/__tests__/components/ui/logo/MatchFaceLogo.test.tsx
@@ -1,7 +1,7 @@
 import MatchFaceLogo from "@/components/ui/logo/MatchFaceLogo";
 import { render } from "@testing-library/react";
 
-describe("ui/logo/MatchFacelogo.tsx", () => {
+describe("MatchFacelogo.tsx", () => {
   it("スナップショット", () => {
     const { container } = render(<MatchFaceLogo />);
     expect(container).toMatchSnapshot();
