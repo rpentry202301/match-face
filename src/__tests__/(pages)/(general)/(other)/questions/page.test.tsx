313ce4f8b959602207a731c42d621be569f623ea
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-07-13 14:16:14 +0900

    テスト途中、jest.fn追加

--- src/__tests__/(pages)/(general)/(other)/questions/page.test.tsx
+++ src/__tests__/(pages)/(general)/(other)/questions/page.test.tsx
@@ -2,9 +2,12 @@ import QuestionsPage from "@/app/(pages)/(general)/(other)/questions/page";
 import { render, screen, within } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { data } from "@/const/questions";
-import { userEvent } from "@testing-library/user-event/dist/types/setup";
 
 describe("一般ユーザー質問一覧画面", () => {
+  // const user = userEvent.setup();
+  // beforeEach(() => {
+  //   render(<QuestionsPage />);
+  // });
   describe("テーブル", () => {
     it("取得データの要素数+1個(見出しのtr)の<tr>が存在する", () => {
       render(<QuestionsPage />);
@@ -14,6 +17,7 @@ describe("一般ユーザー質問一覧画面", () => {
       expect(element).toHaveLength(dataNumbers + 1);
     });
     it("回答期日が表示される", () => {
+      render(<QuestionsPage />);
       const element = screen.getByText(data[0].answer_deadline);
       screen.debug(element);
       expect(element).toBeInTheDocument();
@@ -21,15 +25,17 @@ describe("一般ユーザー質問一覧画面", () => {
   });
   describe("ボタン", () => {
     it("true時のボタンの色が緑である&&「確認する」", () => {
+      render(<QuestionsPage />);
       const element = document.getElementById(`button-true-${data[0].id}`);
       console.log(element);
       expect(element).toHaveClass("bg-green");
       expect(element).toHaveTextContent("確認する");
     });
-    it("確認するボタンにLink先がある", () => {
-      const element = document.getElementById(`button-true-${data[0].id}`);
+    it("確認するボタンにresult/[id]へのLinkがある", () => {
+      render(<QuestionsPage />);
+      const element = screen.getByTestId(`confirmButton${data[0].id}`);
       console.log(element);
-      expect(element).toHaveAttribute("href", "/result/");
+      expect(element).toHaveAttribute("href", `result/${data[0].id}`);
     });
   });
 });
