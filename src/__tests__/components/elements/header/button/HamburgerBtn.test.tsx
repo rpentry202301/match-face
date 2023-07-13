9ebbbd11197f103b0bfde36b31d0e333d6a553d1
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-13 15:31:00 +0900

    途中保存

--- src/__tests__/components/elements/header/button/HamburgerBtn.test.tsx
+++ src/__tests__/components/elements/header/button/HamburgerBtn.test.tsx
@@ -1,13 +1,38 @@
 // Todo: UserIcon非同期通信のモック
 
+import HamburgerBtn from "@/components/elements/header/button/HamburgerBtn";
+import UserIcon from "@/components/elements/header/nav/UserIcon";
+import { fireEvent, render, screen } from "@testing-library/react";
+
 describe("HamburgerBtn.tsx", () => {
-  beforeEach(() => {
+  beforeAll(() => {
     jest.clearAllMocks();
   });
   afterAll(() => {
     jest.clearAllMocks();
   });
-  it("スナップショットテスト", () => {
 
+  describe("スナップショットテスト", () => {
+    it("デフォルト(ナビゲーションウィンドウ閉)", async () => {
+      // UserIcon.tsxのモック
+      const MockUserIcon = await UserIcon();
+      const { container } = render(<HamburgerBtn>{MockUserIcon}</HamburgerBtn>);
+      expect(container).toMatchSnapshot();
+    });
+  });
+  describe("ナビゲーションウィンドウ展開テスト", () => {
+    beforeEach(async () => {
+      const MockUserIcon = await UserIcon();
+      render(<HamburgerBtn>{MockUserIcon}</HamburgerBtn>);
+    });
+    it("ナビゲーションウィンドウ開く→閉じる", async () => {
+      // デフォルト状態との差分でテストする
+      const MockUserIcon = await UserIcon();
+      const DefaultRender = render(
+        <HamburgerBtn>{MockUserIcon}</HamburgerBtn>
+      ).container;
+      // ナビゲーションウィンドウを開く
+      fireEvent.click(screen.getByRole("button"));
+    });
   });
 });
