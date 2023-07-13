e658b5796804df59b5d4b8f615f40089e6447034
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-07-13 10:52:53 +0900

    テスト途中

--- src/__tests__/(pages)/(general)/(other)/questions/page.test.tsx
+++ src/__tests__/(pages)/(general)/(other)/questions/page.test.tsx
@@ -2,43 +2,34 @@ import QuestionsPage from "@/app/(pages)/(general)/(other)/questions/page";
 import { render, screen, within } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { data } from "@/const/questions";
+import { userEvent } from "@testing-library/user-event/dist/types/setup";
 
-describe("テーブル", () => {
-  const testData = {
-    id: "1",
-    project_name: "ポムポムプリン",
-    project_detail:
-      "日本のサンリオによるゴールデン・レトリバーの男のコをモチーフにしたキャラクター。",
-    answer_deadline: "2023/9/15",
-    answer_status: true,
-  };
-  it("取得データの要素数+1個(見出しのtr)の<tr>が存在する", () => {
-    render(<QuestionsPage />);
-    const dataNumbers = data.length;
-    const element = screen.getAllByRole("row");
-    screen.debug(element);
-    expect(element).toHaveLength(dataNumbers + 1);
+describe("一般ユーザー質問一覧画面", () => {
+  describe("テーブル", () => {
+    it("取得データの要素数+1個(見出しのtr)の<tr>が存在する", () => {
+      render(<QuestionsPage />);
+      const dataNumbers = data.length;
+      const element = screen.getAllByRole("row");
+      screen.debug(element);
+      expect(element).toHaveLength(dataNumbers + 1);
+    });
+    it("回答期日が表示される", () => {
+      const element = screen.getByText(data[0].answer_deadline);
+      screen.debug(element);
+      expect(element).toBeInTheDocument();
+    });
   });
-  it("回答期日が表示される", () => {
-    render(<QuestionsPage />);
-    const element = screen.getByText(testData.answer_deadline);
-    screen.debug(element);
-    expect(element).toBeInTheDocument();
-  });
-});
-describe("ボタン", () => {
-  const testData = {
-    id: "1",
-    project_name: "ポムポムプリン",
-    project_detail:
-      "日本のサンリオによるゴールデン・レトリバーの男のコをモチーフにしたキャラクター。",
-    answer_deadline: "2023/9/15",
-    answer_status: true,
-  };
-  it("true時のボタンの色が緑である", () => {
-    render(<QuestionsPage />);
-    const element = document.getElementById(`button-true-${testData.id}`);
-    console.log(element);
-    expect(element).toHaveClass("bg-green");
+  describe("ボタン", () => {
+    it("true時のボタンの色が緑である&&「確認する」", () => {
+      const element = document.getElementById(`button-true-${data[0].id}`);
+      console.log(element);
+      expect(element).toHaveClass("bg-green");
+      expect(element).toHaveTextContent("確認する");
+    });
+    it("確認するボタンにLink先がある", () => {
+      const element = document.getElementById(`button-true-${data[0].id}`);
+      console.log(element);
+      expect(element).toHaveAttribute("href", "/result/");
+    });
   });
 });
