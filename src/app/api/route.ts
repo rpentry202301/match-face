077ee4d55b31411abdb8a6b636770b9dd9b43730
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-07-14 10:48:23 +0900

    修正

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,5 @@
 import { NextResponse } from 'next/server';
-import users from '@/const/login';
 
-// export const GET = async () => {
-//   return NextResponse.json('Hello, World!');
-// };
-
-// export const GET = async () => {
-//   return NextResponse.json(users);
-// };
+export const GET = async () => {
+  return NextResponse.json('Hello, World!');
+};
