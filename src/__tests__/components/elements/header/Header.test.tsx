956e1603b76a14d216774aae791c140c7ee5ac83
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-14 11:12:26 +0900

    Header.tsxのテスト実装

--- src/__tests__/components/elements/header/Header.test.tsx
+++ src/__tests__/components/elements/header/Header.test.tsx
@@ -1,3 +1,26 @@
+import Header from "@/components/elements/header/Header";
+import { cleanup, render } from "@testing-library/react";
+import UserIcon from "@/components/elements/header/nav/UserIcon";
+
+// ServerComponentがJestでエラーを吐くので、UserIcon.tsxをモック
+jest.mock("@/components/elements/header/nav/UserIcon", () => {
+  const mockUserIcon = () => {
+    return <div>ユーザーアイコン</div>;
+  };
+  return mockUserIcon;
+});
+
 describe("Header.tsx", () => {
-    
+  beforeAll(async () => {
+    jest.clearAllMocks();
+    cleanup();
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+    cleanup();
+  });
+  it("スナップショットテスト", () => {
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
 });
