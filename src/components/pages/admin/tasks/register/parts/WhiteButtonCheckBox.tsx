d122487bb58736540313cc8a2834e66dc5f48bbc
YukiNakajima <yuki.nakajima@rakus-partners.co.jp>
2023-07-14 15:43:35 +0900

    WhiteButtonCheckBoxにcursor-pointerを追加

--- src/components/pages/admin/tasks/register/parts/WhiteButtonCheckBox.tsx
+++ src/components/pages/admin/tasks/register/parts/WhiteButtonCheckBox.tsx
@@ -4,12 +4,12 @@ import { twMerge } from "tailwind-merge";
 
 // checkboxでボタンを実装
 const WhiteButtonCheckBox = ({ id, label, ...props }: W_BtnProps) => {
-  const baseStyle = "text-xs inline-flex items-center justify-center py-2 px-4 bg-white hover:bg-gray-100 border rounded-full shadow-md cursor-pointer"
+  const baseStyle = "text-xs inline-flex items-center justify-center py-2 px-4 bg-white hover:bg-gray-100 border rounded-full shadow-md"
   const mergeStyle = twMerge(baseStyle + props.className);
   return (
     <div>
-      <input {...props} type="checkbox" id={id} className={"peer sr-only"} />
-      <label htmlFor={id} className={`${mergeStyle} peer-checked:shadow-sm peer-checked:bg-gray-200 peer-checked:translate-y-0.5`}>
+      <input {...props} type="checkbox" id={id} className={"peer hidden"} />
+      <label htmlFor={id} className={`${mergeStyle} cursor-pointer peer-checked:shadow-sm peer-checked:bg-gray-200 peer-checked:translate-y-0.5`}>
         {label}
       </label>
     </div>
