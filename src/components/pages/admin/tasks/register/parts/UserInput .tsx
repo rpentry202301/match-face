84feb51b1e3c6b1d6d7d441097fecaac84e74955
YukiNakajima <yuki.nakajima@rakus-partners.co.jp>
2023-07-18 14:11:04 +0900

    useUserSelectの実装

---
+++ src/components/pages/admin/tasks/register/parts/UserInput .tsx
@@ -0,0 +1,14 @@
+'use client'
+import Input from "@/components/ui/Input"
+
+const UserInput = () => {
+  return (
+    <Input
+      id="search"
+      className="border-2 border-light-gray text-xs p-1 w-full"
+      readOnly
+    />
+  )
+}
+
+export default UserInput
