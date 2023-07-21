17c29e71be70b096b54e2a4ae45e2b06830bdd47
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-07-21 10:48:21 +0900

    見た目（完了）、レンダリングの制御

--- src/app/(pages)/(general)/(other)/histories/page.tsx
+++ src/app/(pages)/(general)/(other)/histories/page.tsx
@@ -3,13 +3,13 @@
 import HistoryList from "@/components/pages/general/histories/HistoryList";
 import OrangeButton from "@/components/ui/button/OrangeButton";
 import { skills } from "@/const/histories";
+import React from "react";
 import { useState } from "react";
 
-// ここでuseStateで絞り込み条件データを保持,Listに値を渡してListコンポで絞り込み
-// または、このpageから selectコンポへ関数呼び出し、 selectで値を確定してpageのuseStateを変更、Listへ渡す
 const HistoriesPage = () => {
   const [month, setMonth] = useState("");
   const [selectSkills, setSelectSkills] = useState([]);
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const deadlines = [
     { id: 1, value: "2023-07" },
@@ -17,6 +17,12 @@ const HistoriesPage = () => {
     { id: 3, value: "2023-03" },
   ];
 
+  const handleMonthChange = (e) => {
+    setMonth(e.target.value);
+    setIsSubmit(false);
+  };
+
+  // 技術チェックボックスが押下された時の挙動
   const handleSkillChange = (e) => {
     const skillId = parseInt(e.target.value);
     if (e.target.checked) {
@@ -28,58 +34,71 @@ const HistoriesPage = () => {
         prevSkills.filter((id) => id !== skillId)
       );
     }
+    setIsSubmit(false);
   };
 
+  // Submitボタンが押下された時の処理
   const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log(month, selectSkills);
+    setIsSubmit(true);
   };
+  console.log("親レンダリング");
+
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <div className="mb-[2vh]">
-          <label htmlFor="month">回答月：</label>
-          <select
-            name="month"
-            id="month"
-            className="border-2"
-            onChange={(e) => setMonth(e.target.value)}
+      <div className="flex flex-wrap justify-around">
+        <section className="w-[75vw] border-2 text-center my-[5vh] py-[5vh]">
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
           >
-            <option value="">--</option>
-            {deadlines.map((deadline) => (
-              <option value={deadline.value} key={deadline.id}>
-                {deadline.value}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="lg:flex justify-center mb-[2vh] max-w-[55vw] ml-[10vw]">
-          <legend className="lg:w-[215px] lg:h-10 lg:leading-10">
-            使用技術：
-          </legend>
-          <fieldset id="skill" name="skill">
-            {skills.map((skill) => (
-              <span key={skill.id} className="px-[10px] whitespace-nowrap">
-                <input
-                  type="checkbox"
-                  id={skill.skill}
-                  name={skill.skill}
-                  value={skill.id}
-                  onChange={handleSkillChange}
-                />
-                <label htmlFor={skill.skill}>{skill.skill}</label>
-              </span>
-            ))}
-          </fieldset>
-        </div>
-        <OrangeButton label="絞り込み" type="submit" />
-      </form>
+            <div className="mb-[2vh]">
+              <label htmlFor="month">回答月：</label>
+              <select
+                name="month"
+                id="month"
+                className="border-2"
+                onChange={handleMonthChange}
+              >
+                <option value="">--</option>
+                {deadlines.map((deadline) => (
+                  <option value={deadline.value} key={deadline.id}>
+                    {deadline.value}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="lg:flex justify-center mb-[2vh] max-w-[55vw] ml-[10vw]">
+              <legend className="lg:w-[215px] lg:h-10 lg:leading-10">
+                使用技術：
+              </legend>
+              <fieldset id="skill" name="skill">
+                {skills.map((skill) => (
+                  <span key={skill.id} className="px-[10px] whitespace-nowrap">
+                    <input
+                      type="checkbox"
+                      id={skill.skill}
+                      name={skill.skill}
+                      value={skill.id}
+                      onChange={handleSkillChange}
+                    />
+                    <label htmlFor={skill.skill}>{skill.skill}</label>
+                  </span>
+                ))}
+              </fieldset>
+            </div>
+            <OrangeButton label="絞り込み" type="submit" />
+          </form>
+        </section>
+      </div>
       <div className="flex flex-col items-center h-screen ">
-        <HistoryList month={month} skill={selectSkills} />
+        {isSubmit ? (
+          <HistoryList month={month} skill={selectSkills} />
+        ) : (
+          <HistoryList />
+        )}
       </div>
     </>
   );
--- src/components/pages/general/histories/HistoryList.tsx
+++ src/components/pages/general/histories/HistoryList.tsx
@@ -4,107 +4,104 @@ import GrayButton from "@/components/ui/button/GrayButton";
 import OrangeButton from "@/components/ui/button/OrangeButton";
 import { projects } from "@/const/histories";
 import Link from "next/link";
-
-type Data = {
-  id: number;
-  comment_created_at: boolean;
-  name: string;
-  detail: string;
-  answer_update_at: string;
-  skill_id: number[];
-}[];
+import { memo } from "react";
 
 interface HistoryListProps {
-  month: string;
-  skill: number[];
+  month?: string;
+  skill?: number[] | undefined;
 }
 
-const HistoryList: React.FC<HistoryListProps> = ({ month, skill }) => {
-  // timestampからyyy/mmの形にする
-  // const timestamp = projects[0].answer_update_at;
-  // const newDate = new Date(timestamp);
-  // const getYear = newDate.getFullYear();
-  // const getMonth = newDate.getMonth() + 1;
-  // const formattedDate = `${getYear}/${String(getMonth).padStart(2, "0")}`;
-  // console.log("月", formattedDate);
+const HistoryList: React.FC<HistoryListProps> = memo(
+  // 引数が渡されなかった場合のデフォルト引数
+  ({ month = "", skill = [] }) => {
+    // timestampからyyy/mmの形にする
+    // const timestamp = projects[0].answer_update_at;
+    // const newDate = new Date(timestamp);
+    // const getYear = newDate.getFullYear();
+    // const getMonth = newDate.getMonth() + 1;
+    // const formattedDate = `${getYear}/${String(getMonth).padStart(2, "0")}`;
+    // console.log("月", formattedDate);
 
-  const truncateString = (str: any, num: any) => {
-    return str.length <= num ? str : str.slice(0, num) + "...";
-  };
+    const truncateString = (str: any, num: any) => {
+      return str.length <= num ? str : str.slice(0, num) + "...";
+    };
 
-  let selectProject;
-  if (skill.length > 0 && month) {
-    selectProject = projects.filter(
-      (project) =>
-        project.skill_id.some((skillId) => skill.includes(skillId)) &&
-        project.answer_update_at.slice(0, 7) === month
-    );
-    console.log("monthとskillが一致", selectProject);
-  } else if (skill.length > 0 && !month) {
-    // いずれかのskillが一致したもの、完全一致ではない
-    selectProject = projects.filter((project) =>
-      project.skill_id.some((skillId) => skill.includes(skillId))
-    );
-    console.log("skill絞り込みプロジェクト", selectProject);
-  } else if (month && skill.length == 0) {
-    selectProject = projects.filter(
-      (project) => project.answer_update_at.slice(0, 7) === month
-    );
-    console.log("month絞り込みプロジェクト", selectProject);
-  } else {
-    selectProject = projects.filter((project) => project.id !== 0);
-    console.log("全プロジェクト", selectProject);
-  }
+    let selectProject;
+    if (skill.length > 0 && month) {
+      selectProject = projects.filter(
+        (project) =>
+          project.skill_id.some((skillId) => skill.includes(skillId)) &&
+          project.answer_update_at.slice(0, 7) === month
+      );
+      console.log("monthとskillが一致", selectProject);
+    } else if (skill.length > 0 && !month) {
+      // いずれかのskillが一致したもの、完全一致ではない
+      selectProject = projects.filter((project) =>
+        project.skill_id.some((skillId) => skill.includes(skillId))
+      );
+      console.log("skill絞り込みプロジェクト", selectProject);
+    } else if (month && skill.length == 0) {
+      selectProject = projects.filter(
+        (project) => project.answer_update_at.slice(0, 7) === month
+      );
+      console.log("month絞り込みプロジェクト", selectProject);
+    } else {
+      selectProject = projects.filter((project) => project.id !== 0);
+      console.log("全プロジェクト", selectProject);
+    }
 
-  return (
-    <div>
-      <table className="table-auto border border-collapse my-20 w-[80vw]">
-        <thead>
-          <tr>
-            <th className="border p-4 bg-neutral-300">コメント</th>
-            <th className="border  bg-neutral-300">回答日</th>
-            <th className="border  bg-neutral-300">案件名</th>
-            <th className="border  bg-neutral-300">案件概要</th>
-            <th className="border  bg-neutral-300"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {selectProject.map((project) => (
-            <tr key={project.id}>
-              <td className="border text-center p-3">
-                {project.comment_created_at ? (
-                  <span className=" bg-orange text-white p-2 w-40 h-auto w-15 rounded py-1 px-2 text-xs">
-                    新着
-                  </span>
-                ) : (
-                  ""
-                )}
-              </td>
-              <td className="border text-center p-3">
-                {project.answer_update_at}
-              </td>
-              <td className="border text-center px-4">{project.name}</td>
-
-              <td
-                className="border text-center px-4 "
-                data-test={`projectDetail${project.id}`}
-              >
-                {truncateString(project.detail, 30)}
-              </td>
-              <td className="border text-center px-4">
-                <Link href={`result/${project.id}`}>
-                  <GrayButton
-                    label={"詳細"}
-                    className="w-15 rounded py-1 px-2 text-xs"
-                    value={project.id}
-                  />
-                </Link>
-              </td>
+    console.log("子レンダリング");
+    return (
+      <div>
+        <table className="table-auto border border-collapse my-20 w-[80vw]">
+          <thead>
+            <tr>
+              <th className="border p-4 bg-neutral-300">コメント</th>
+              <th className="border  bg-neutral-300">回答日</th>
+              <th className="border  bg-neutral-300">案件名</th>
+              <th className="border  bg-neutral-300">案件概要</th>
+              <th className="border  bg-neutral-300"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+          </thead>
+          <tbody>
+            {selectProject.map((project) => (
+              <tr key={project.id}>
+                <td className="border text-center p-3">
+                  {project.comment_created_at ? (
+                    <span className=" bg-orange text-white p-2 w-40 h-auto w-15 rounded py-1 px-2 text-xs">
+                      新着
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                </td>
+                <td className="border text-center p-3">
+                  {project.answer_update_at}
+                </td>
+                <td className="border text-center px-4">{project.name}</td>
+
+                <td
+                  className="border text-center px-4 "
+                  data-test={`projectDetail${project.id}`}
+                >
+                  {truncateString(project.detail, 30)}
+                </td>
+                <td className="border text-center px-4">
+                  <Link href={`result/${project.id}`}>
+                    <GrayButton
+                      label={"詳細"}
+                      className="w-15 rounded py-1 px-2 text-xs"
+                      value={project.id}
+                    />
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+);
+HistoryList.displayName = "HistoryList";
 export default HistoryList;
