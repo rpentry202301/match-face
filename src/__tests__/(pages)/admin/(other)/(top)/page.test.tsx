8a534d1f4cb65d38e8c1431c89a0ecdf202aa40d
mizuki-nakagawa <mizuki.nakagawa@rakus-partners.co.jp>
2023-07-21 09:14:39 +0900

    admiトップ・ユーザー管理画面のテスト修正、style修正

--- src/__tests__/(pages)/admin/(other)/(top)/page.test.tsx
+++ src/__tests__/(pages)/admin/(other)/(top)/page.test.tsx
@@ -3,7 +3,9 @@ import { render, screen } from "@testing-library/react";
 import AdminHome from "@/app/(pages)/admin/(other)/(top)/page";
 import React from "react";
 import "@testing-library/jest-dom";
+import { dataArray } from "@/const/adminTop";
 
+const user = userEvent.setup();
 describe("adminトップページのテスト", () => {
   beforeEach(() => {
     render(<AdminHome />);
@@ -13,13 +15,35 @@ describe("adminトップページのテスト", () => {
     expect(container).toMatchSnapshot();
   });
 
-  // PASSしないので一旦保留
-  describe("メニューテスト", () => {
-    it("メニューをクリックすると、対象のページへ遷移する", async () => {
-      const menuButton = screen.getByTestId("menu_1");
-      await userEvent.click(menuButton);
-      const currentUrl = window.location.pathname;
-      expect(currentUrl).toContain("/admin/handle-question");
-    });
+  it("データとメニューの数が等しい", async () => {
+    const menuComponents = screen.getAllByTestId("menu_link");
+    // console.log(menuComponents.length);
+    expect(menuComponents.length).toBe(dataArray.length);
+  });
+  it("メニューが正しく表示される", () => {
+    const title = screen.getByText(dataArray[0].title);
+    const url = document.querySelector(`a[href="${dataArray[0].url}"]`);
+    const description = screen.getByText(dataArray[0].description);
+    const img = document.querySelector("img");
+    // console.log(img);
+    const imgAlt = img?.getAttribute("alt");
+    // console.log(imgAlt);
+    const imgUrl = img?.getAttribute("src");
+    // console.log(imgUrl);
+
+    expect(title).toBeInTheDocument();
+    expect(url).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(imgAlt).toContain(dataArray[0].imgAlt);
+    expect(imgUrl).toContain("document_icon.png");
+  });
+
+  it("メニュークリックでページ遷移", () => {
+    const menu = document.querySelector(
+      `a[href="${dataArray[0].url}"]`
+    ) as HTMLElement;
+    // console.log(menu);
+    user.click(menu);
+    expect(menu).toHaveAttribute("href", "/admin/handle-question");
   });
 });
