559b0357b934b7186a889437ebaf3ceb1f6a88f1
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2023-07-25 11:53:14 +0900

    ワード絞り込み機能＆テスト

--- src/__tests__/(pages)/admin/(other)/handle-question/page.test.tsx
+++ src/__tests__/(pages)/admin/(other)/handle-question/page.test.tsx
@@ -55,4 +55,36 @@ describe("質問・回答例一覧画面テスト", () => {
     //テーブル内にJava属性を持たない行が表示されていないか
     expect(table).not.toContainElement(nonExistElement)
   });
+
+  it("PHP絞り込み&ワード検索実行", async () => {
+    const addWriteButton = screen.getByTestId("button-PHP");
+    const searchInput = screen.getByTestId("input-search");
+    const refineButton = screen.getByTestId("button-refine");
+    const table = screen.getByTestId("projectTable");
+    const descendant = screen.getByTestId("test3");
+    const nonExistElement = screen.getByTestId("test4");
+    fireEvent.change(searchInput, { target: { value: "test3" } });
+    fireEvent.click(addWriteButton);
+    fireEvent.click(refineButton);
+    //テーブル内にPHP属性かつtest3の行が表示されているか
+    expect(table).toContainElement(descendant)
+    expect(descendant).not.toContainElement(table)
+    //テーブル内にPHP属性かつtest3を持たない行が表示されていないか
+    expect(table).not.toContainElement(nonExistElement)
+  });
+
+  it("ワード検索実行", async () => {
+    const searchInput = screen.getByTestId("input-search");
+    const refineButton = screen.getByTestId("button-search");
+    const table = screen.getByTestId("projectTable");
+    const descendant = screen.getByTestId("test3");
+    const nonExistElement = screen.getByTestId("test1");
+    fireEvent.change(searchInput, { target: { value: "test3" } });
+    fireEvent.click(refineButton);
+    //テーブル内にtest3の行が表示されているか
+    expect(table).toContainElement(descendant)
+    expect(descendant).not.toContainElement(table)
+    //テーブル内にtest3を持たない行が表示されていないか
+    expect(table).not.toContainElement(nonExistElement)
+  });
 });
