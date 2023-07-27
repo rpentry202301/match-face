762ae2c82aaec5696e6c08d07e37926c790ae178
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-07-27 11:51:11 +0900

    WhiteCheckButton.tsxでonClickが使えない問題を解消

--- src/components/pages/admin/tasks/index/parts/SearchByJobs.tsx
+++ src/components/pages/admin/tasks/index/parts/SearchByJobs.tsx
@@ -1,17 +1,36 @@
-'use client';
+"use client";
 import Input from "@/components/ui/Input";
 import OrangeButton from "@/components/ui/button/OrangeButton";
 import WhiteButton from "@/components/ui/button/WhiteButton";
 import WhiteCheckButton from "@/components/ui/button/WhiteCheckButton";
+import { useState } from "react";
 
 // 削除予定
 import { departments } from "@/const/tasks";
 
 const SearchByJobs = () => {
+  const [jobsFilter, setJobsFilter] = useState<string[]>([]);
+
+  const handleSetFilter = (department: string) => {
+    // filter配列に職種レコードを挿入
+    if (!jobsFilter.includes(department)) {
+      const newArr = jobsFilter;
+      newArr.push(department);
+      setJobsFilter(newArr);
+    } else {
+      // filter配列から職種レコードを削除
+      const newArr = jobsFilter.filter((job) => {
+        return job !== department;
+      });
+      setJobsFilter(newArr);
+    }
+    console.log("jobsFilter", jobsFilter);
+  };
+
   return (
     <div className="flex flex-col items-center border-2 rounded-md w-3/6 mx-auto mt-6 p-3">
       <div className="flex items-center mb-4">
-        <Input id="search" className="border-light-gray text-xs p-1 w-96"/>
+        <Input id="search" className="border-light-gray text-xs p-1 w-96" />
         <WhiteButton label="検索" className="text-xs ml-2" />
       </div>
       <div className="flex items-center mb-4">
@@ -19,7 +38,11 @@ const SearchByJobs = () => {
         {departments.map((department) => {
           return (
             <div key={department.id} className="mx-3">
-              <WhiteCheckButton label={department.name} className="text-xs w-16" />
+              <WhiteCheckButton
+                label={department.name}
+                className="text-xs w-16"
+                onClick={() => handleSetFilter(department.name)}
+              />
             </div>
           );
         })}
