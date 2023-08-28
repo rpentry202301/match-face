803fb2d449f9c41d52f30473b39db63e3b1ec7ae
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-08-28 16:42:12 +0900

    バックエンドからデータ取得実装

--- src/app/(pages)/(general)/(other)/questions/page.tsx
+++ src/app/(pages)/(general)/(other)/questions/page.tsx
@@ -1,23 +1,49 @@
 import AnswerButton from "@/components/pages/general/questions/AnswerButton";
-import { data } from "@/const/questions";
 import Link from "next/link";
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  PromiseLikeOfReactNode,
+} from "react";
 
-type ProjectData = {
-  id: string;
-  project_name: string;
-  project_detail: string;
-  answer_deadline: string;
-  answer_status: boolean;
-}[];
+type ProjectType = {
+  id: Key;
+  deadline: string;
+  project: {
+    name: string;
+    detail: string;
+  };
+  answered: boolean;
+};
 
-const QuestionsPage = () => {
-  // const response = await fetch('http://localhost:3000/api');
-  // if (!response.ok) throw new Error('Failed to fetch data');
-  // const projectData: ProjectData = await response.json();
-  const projectData: ProjectData = data;
+const QuestionsPage = async () => {
+  const response = await fetch("http://localhost:3000/api", {
+    cache: "no-store",
+  });
+  if (!response.ok) throw new Error("Failed to fetch data");
+  const FetchData = await response.json();
+  const answerRequestList = FetchData.answerRequestList;
+  // console.log("取得したデータ", answerRequestList);
+
+  //  案件詳細の文字数制限
   const truncateString = (str: string, num: number) => {
     return str.length <= num ? str : str.slice(0, num) + "...";
   };
+
+  // 回答期限の表示
+  const formedDeadline = (inputData: string): string => {
+    const date = new Date(inputData);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}年${month < 10 ? "0" : ""}${month}月${
+      day < 10 ? "0" : ""
+    }${day}日`;
+  };
+
   return (
     <div className="flex flex-col items-center h-screen ">
       <table className="table-auto border border-collapse my-20 w-3/4">
@@ -30,28 +56,28 @@ const QuestionsPage = () => {
           </tr>
         </thead>
         <tbody>
-          {projectData.map((project) => (
+          {answerRequestList.map((project: ProjectType) => (
             <tr key={project.id}>
               <td className="border text-center p-3">
-                {project.answer_deadline}
+                {formedDeadline(project.deadline)}
               </td>
               <td className="border text-center px-4">
-                {project.project_name}
+                {project.project.name}
               </td>
               <td className="border text-center px-4">
-                {truncateString(project.project_detail, 30)}
+                {truncateString(project.project.detail, 30)}
               </td>
               <td className="border text-center px-4 ">
-                {project.answer_status ? (
+                {project.answered ? (
                   <Link
                     href={`result/${project.id}`}
                     data-testid={`confirmButton${project.id}`}
                   >
-                    <AnswerButton answered={project.answer_status} />
+                    <AnswerButton answered={project.answered} />
                   </Link>
                 ) : (
                   <Link href={`testing/${project.id}`}>
-                    <AnswerButton answered={project.answer_status} />
+                    <AnswerButton answered={project.answered} />
                   </Link>
                 )}
               </td>
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,9 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  return NextResponse.json('Hello, World!');
+  const response = await fetch(
+    "http://localhost:8080/qa_system_api/user/1/answer_requests/deadline"
+  );
+  const data = await response.json();
+  return NextResponse.json(data);
 };
