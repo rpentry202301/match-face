a05df694b2d6527ca6be6971841ce5bd69b1893a
Takao Shishido <ckaikd180.0512@gmail.com>
2023-09-06 17:07:40 +0900

    nextauthの導入

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const response = await fetch(
+          `http://localhost:8080/qa_system_api/users/${credentials?.email}?password=${credentials?.password}`,
+          {
+            cache: 'no-store',
+            method: 'GET',
+            // headers: {
+            //   'Content-Type': 'application/json',
+            // },
+            // body: JSON.stringify({
+            //   userId: 1,
+            //   password: 'testtest',
+            // }),
+          }
+        );
+        const userData = await response.json();
+        const user = userData.user;
+        console.log('user', user);
+        if (user && user.email === credentials?.email) {
+          return user;
+        } else if (userData.length === 0) {
+          return null;
+        }
+      },
+    }),
+  ],
+  //   pages: {
+  //     signIn: '/login',
+  //   },
+});
+export { handler as GET, handler as POST };
