eb96eb364d73ebde81b773b8cc3cf279ccea2fd4
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-09-06 17:09:01 +0900

    途中

--- src/__tests__/(pages)/(general)/(other)/questions/page.test.tsx
+++ src/__tests__/(pages)/(general)/(other)/questions/page.test.tsx
@@ -2,22 +2,20 @@ import QuestionsPage from "@/app/(pages)/(general)/(other)/questions/page";
 import { render, screen, cleanup, act, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { data } from "@/const/questions";
+import { mockGetData } from "./mock";
 
-//ユーザのイベントをテストするため
-import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
-// ダミーAPI作成用
-import { rest, RequestHandler, setupWorker } from "msw";
-import { setupServer } from "msw/node";
-import "cross-fetch/polyfill";
+// //ユーザのイベントをテストするため
+// import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
+// // ダミーAPI作成用
+// import { rest, RequestHandler, setupWorker } from "msw";
+// import { setupServer } from "msw/node";
+// import "cross-fetch/polyfill";
 
-beforeAll(() => server.listen());
-// afterEach(() => {
-//   cleanup();
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => {
+//   server.close();
 // });
-afterEach(() => server.resetHandlers());
-afterAll(() => {
-  server.close();
-});
 // const server = setupServer(
 //   rest.post("/api/questions", async (req, res, ctx) => {
 //     const { user_id } = await req.json();
@@ -37,14 +35,14 @@ afterAll(() => {
 //   })
 // );
 
-test("allows user to log in", async () => {
-  // Render components, perform requests, receive mocked responses.
-});
+jest.mock("../../../../../app/api/(general)/questions/route");
 
 describe("スナップショットテスト", () => {
-  it("レンダリング時", async () => {
-    render(<QuestionsPage />);
-    expect(screen).toMatchSnapshot();
+  it("データ取得後の質問一覧画面が表示される", async () => {
+    const mockFn = mockGetData();
+    const { container } = render(<QuestionsPage />);
+    expect(mockFn).toBeCalled();
+    expect(container).toMatchSnapshot();
   });
 });
 describe("一般ユーザー質問一覧画面", () => {
