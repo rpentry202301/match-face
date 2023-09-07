2e372580b6df6a4ddee2855a0999f0bf658089a2
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-09-07 11:27:33 +0900

    レイアウトを調整

--- src/components/pages/admin/tasks/index/parts/SearchByJobs.tsx
+++ src/components/pages/admin/tasks/index/parts/SearchByJobs.tsx
@@ -4,28 +4,25 @@ import OrangeButton from "@/components/ui/button/OrangeButton";
 import WhiteButton from "@/components/ui/button/WhiteButton";
 import WhiteCheckButton from "@/components/ui/button/WhiteCheckButton";
 import { SyntheticEvent, useState } from "react";
-import { useFilter } from "@/hooks/store/context/TasksContext";
 
 // 削除予定
 import { departments } from "@/const/tasks";
+import Link from "next/link";
 
 const SearchByJobs = () => {
   // 職種フィルター状態管理
   const [jobsFilter, setJobsFilter] = useState<string[]>([]);
-  const [filterList, setFilterList] = useFilter(); // context
   // 検索入力値状態管理
   const [inputVal, setInputVal] = useState<string>("");
 
   const handleSearch = (e: SyntheticEvent) => {
     e.preventDefault();
     if (inputVal.length === 0) {
-      setFilterList({ ...filterList, search: [] });
       return;
     }
     const wordList = inputVal
       .split(/[\s]+/)
       .filter((word) => word.length !== 0);
-    setFilterList({ ...filterList, search: wordList });
   };
 
   const handleSetFilter = (department: string) => {
@@ -44,47 +41,48 @@ const SearchByJobs = () => {
     // console.log("jobsFilter2", jobsFilter);
   };
 
-  // フィルターを適用
-  const handleApplyFilter = () => {
-    setFilterList({
-      ...filterList,
-      departments: jobsFilter,
-    });
-  };
-
   return (
-    <div className="flex flex-col items-center border-2 rounded-md w-3/6 mx-auto mt-6 p-3">
-      <div>
-        <form onSubmit={handleSearch} className="flex items-center mb-4">
+    <div className="border-2 rounded-md w-3/6 mx-auto mt-6 p-3">
+      {/* 案件名/ユーザー名検索欄 */}
+      <div className="w-full">
+        <div className="mb-2">
+          <h2 className="text-base ml-3">▶️案件名/ユーザー名で絞り込み</h2>
+        </div>
+        <form onSubmit={handleSearch} className="flex flex-col items-center mb-4">
           <Input
             id="search"
-            className="border-light-gray text-xs p-1 w-96"
+            className="border-light-gray text-xs p-1 w-[35rem]"
             onChange={(e) => setInputVal(e.target.value)}
             data-testid="search-box"
           />
-          <WhiteButton label="検索" className="text-xs ml-2" type="submit" />
+          <Link href={`/admin/tasks`} className="mt-2">
+            <OrangeButton label="検索" className="w-20 text-sm" />
+          </Link>
         </form>
       </div>
-      <div className="flex items-center mb-4">
-        {/* ToDo: 職種データを非同期通信でGET */}
-        {departments.map((department) => {
-          return (
-            <div key={department.id} className="mx-3">
+
+      {/* 職種フィルター入力欄 */}
+      <div className="w-full">
+        <div className="mb-2">
+          <h2 className="text-base ml-3">▶️募集職種で絞り込み</h2>
+        </div>
+        <div className="flex justify-center">
+          {departments.map((department) => {
+            return (
               <WhiteCheckButton
+                key={department.id}
                 label={department.name}
-                className="text-xs w-16"
+                className="text-xs w-16 mx-3"
                 onClick={() => handleSetFilter(department.name)}
               />
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <OrangeButton
-          label="絞り込み"
-          className="w-28 text-sm"
-          onClick={handleApplyFilter}
-        />
+            );
+          })}
+        </div>
+        <div className="flex justify-center">
+          <Link href={`/admin/tasks`} className="mt-3">
+            <OrangeButton label="絞り込み" className="w-20 text-sm" />
+          </Link>
+        </div>
       </div>
     </div>
   );
