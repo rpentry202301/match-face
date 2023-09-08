2080f18bbe6557551efdbaa881f02e9ef40a5399
yukatakahashi <yuka.takahashi@rakus-partners.co.jp>
2023-09-08 17:10:12 +0900

    fetchのコードを微修正しました

--- src/app/api/(general)/result/route.ts
+++ src/app/api/(general)/result/route.ts
@@ -4,7 +4,10 @@ export async function POST(request: Request) {
   const req = await request.json();
   const project_id = req.project_id;
   const response = await fetch(
-    `http://localhost:8080/qa_system_api/answer_requests/${project_id}`
+    `${process.env.BE_URL}/answer_requests/${project_id}`,
+    {
+      cache: "no-cache",
+    }
   );
   const data = await response.json();
   return NextResponse.json(data);
