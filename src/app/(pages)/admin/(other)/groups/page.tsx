352f2ec8a5939383c83db395539b7c06c85d8d4f
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-09-09 14:27:42 +0900

    微修正

--- src/app/(pages)/admin/(other)/groups/page.tsx
+++ src/app/(pages)/admin/(other)/groups/page.tsx
@@ -1,13 +1,8 @@
-import GroupTableFromDb from "@/components/pages/admin/groups/GroupTableFromDB";
 import GroupsTable from "@/components/pages/admin/groups/GroupsTable";
 
 const GroupsPage = () => {
   return (
-    <>
-      {/* <GroupTableFromDb /> */}
-      {/* サーバーサイドでデータ取得完了後、こちらを実装したい*/}
       <GroupsTable/>
-    </>
   );
 };
 
