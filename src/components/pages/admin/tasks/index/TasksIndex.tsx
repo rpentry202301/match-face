9df6fcc16be04afe6ad9be1dd1581d359b901577
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-09-11 15:38:18 +0900

    非同期通信を行うコンポーネントを変更

--- src/components/pages/admin/tasks/index/TasksIndex.tsx
+++ src/components/pages/admin/tasks/index/TasksIndex.tsx
@@ -8,7 +8,16 @@ import { Answer_RequestsType, Departments } from "@/types/admin/tasks/types";
  * @author Hayato Kobayashi
  * @todo_1 レスポンシブ対応
  */
-const TasksIndex = async ({ departments, searchParams }: Props) => {
+const TasksIndex = async ({ searchParams }: Props) => {
+  // 職種データを取得
+  const res1 = await fetch(`${process.env.BE_URL}/departments`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const departments: { departmentList: Departments[] } = await res1.json();
+
   // クエリから検索条件{ searchKeyword, departmentId }を取得
   const { searchKeyword, departmentId } = searchParams;
   /**
@@ -44,7 +53,7 @@ const TasksIndex = async ({ departments, searchParams }: Props) => {
   const query = searchKeywordQuery + departmentIdQuery;
 
   // tasksデータの取得
-  const res = await fetch(
+  const res2 = await fetch(
     `${process.env.BE_URL}/answer_requests${query && "?" + query}`,
     {
       method: "GET",
@@ -53,12 +62,12 @@ const TasksIndex = async ({ departments, searchParams }: Props) => {
       },
     }
   );
-  const answer_requests: Answer_RequestsType = await res.json();
+  const answer_requests: Answer_RequestsType = await res2.json();
 
   return (
     <main>
       <div className="mb-8">
-        <SearchByJobs departments={departments} />
+        <SearchByJobs departments={departments.departmentList} />
       </div>
       <div className="flex justify-center mb-8">
         <Link href={"/admin/tasks/register"} data-testid="link_task_register">
@@ -73,7 +82,6 @@ const TasksIndex = async ({ departments, searchParams }: Props) => {
 };
 
 type Props = {
-  departments: Departments[];
   searchParams: {
     departmentId: string | string[] | undefined;
     searchKeyword: string | string[] | undefined;
