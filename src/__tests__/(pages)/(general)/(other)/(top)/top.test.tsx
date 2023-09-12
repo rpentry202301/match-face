dfd018e57deea540beb0795192086ec8fb35a51f
Takao Shishido <ckaikd180.0512@gmail.com>
2023-09-12 17:45:42 +0900

    Menu.tsxのテスト用データをテストファイルに格納 上記に伴いconst以下のテスト用データを削除

--- src/__tests__/(pages)/(general)/(other)/(top)/top.test.tsx
+++ src/__tests__/(pages)/(general)/(other)/(top)/top.test.tsx
@@ -1,6 +1,8 @@
 import Top from '@/app/(pages)/(general)/(other)/(top)/page';
 import { render } from '@testing-library/react';
 
-describe('ユーザートップページのテスト', () => {
-  test('fetchが呼ばれていること', () => {});
-});
+// describe('ユーザートップページのテスト', () => {
+//   test('fetchが呼ばれていること', async () => {
+//     global.fetch = jest.fn().mockImplementation
+//   });
+// });
