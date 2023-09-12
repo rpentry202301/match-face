adb5d3e35ff95cfaf83bff49ab532b8c58ecec42
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-09-12 09:58:14 +0900

    コメント削除

--- src/components/pages/admin/groups/fetchData.tsx
+++ src/components/pages/admin/groups/fetchData.tsx
@@ -2,6 +2,5 @@ export const fetchData = async () => {
 
   const response = await fetch(`http://localhost:3000/api/admin/groups`, { cache: "no-cache" });
   const data = await response.json();
-  console.log('取って来れてる？:',data)
   return data;
 };
