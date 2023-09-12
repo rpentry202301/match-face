07cd53a246a845e7f4d66fbd6414038549f14f3f
Takao Shishido <ckaikd180.0512@gmail.com>
2023-09-12 17:43:04 +0900

    Notification.tsxのテスト用データをテストファイルに格納 上記に伴いconst以下のテスト用データを削除

---
+++ src/__tests__/(pages)/(general)/(other)/(top)/top.test.tsx
@@ -0,0 +1,6 @@
+import Top from '@/app/(pages)/(general)/(other)/(top)/page';
+import { render } from '@testing-library/react';
+
+describe('ユーザートップページのテスト', () => {
+  test('fetchが呼ばれていること', () => {});
+});
