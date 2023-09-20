1d37aaaee52e74aca92c3a5a037e36e3cda689b8
Takao Shishido <ckaikd180.0512@gmail.com>
2023-09-20 15:17:02 +0900

    middlewareの調整、一部不具合の修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -55,5 +55,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
