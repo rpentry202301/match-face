575a1631cb2aa748d2312c80fe9f1fda8f25e338
Takao Shishido <ckaikd180.0512@gmail.com>
2023-09-21 14:50:57 +0900

    ダミーデータをテストファイルに移行

--- src/__tests__/(pages)/(general)/login/page.test.tsx
+++ src/__tests__/(pages)/(general)/login/page.test.tsx
@@ -3,7 +3,52 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { AppRouterContextProviderMock } from '@/__tests__/test_utils/app-router-context-provider-mock';
-import { dummyUser, users } from '@/const/login';
+
+type User = {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+  hire_date: string;
+  department_id: number;
+  status_id: number;
+  created_user: string;
+  created_at: string;
+  update_user: string;
+  update_at: string;
+};
+type Users = User[];
+
+const users: Users = [
+  {
+    id: 1111,
+    name: 'ラクス太郎',
+    password: 'password#1',
+    email: 'rakus@example.com',
+    hire_date: '2023-01-01',
+    department_id: 1,
+    status_id: 1,
+    created_user: 'ラクス責任者',
+    created_at: '2022-12-30',
+    update_user: 'ラクス責任者',
+    update_at: '2023-01-02',
+  },
+];
+const dummyUser: Users = [
+  {
+    id: 999999,
+    name: 'incorrect',
+    password: 'incorrectPass#1',
+    email: 'incorrect@example.com',
+    hire_date: '9999-99-99',
+    department_id: 999999,
+    status_id: 999999,
+    created_user: 'incorrectUser',
+    created_at: '9999-99-99',
+    update_user: 'incorrectUser',
+    update_at: '9999-99-99',
+  },
+];
 
 const push = jest.fn();
 const userEv = userEvent.setup();
@@ -58,6 +103,7 @@ describe('ページがレンダリングされた時', () => {
     );
   });
 
+  // 中断したためコメントアウト
   // test('"パスワードを忘れた"のリンクのurlが"/remind"であること', () => {
   //   //"パスワードを忘れた"のリンク
   //   const remindLink = screen.getByRole('link', { name: 'パスワードを忘れた' });
