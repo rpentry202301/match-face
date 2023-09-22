4031d79ff20c31f7c12d4430674c2e3975eb0d7c
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2023-09-22 13:50:16 +0900

    add test

--- src/__tests__/(pages)/admin/(other)/histories/select.test.tsx
+++ src/__tests__/(pages)/admin/(other)/histories/select.test.tsx
@@ -20,35 +20,35 @@ import { answer_request_groupsMock, departmentsMock, skillsMock } from './histor
     let selectMonth:HTMLOptionElement;
     let selectFR:HTMLInputElement;
     let selectJavaScript:HTMLInputElement;
+    global.fetch = jest.fn().mockImplementation((url,config)=>{
+        if(url === '/api/admin/histories/departments'){
+            return {ok:true,json: async () => (departmentsMock)}
+        }else if(url === '/api/admin/histories/skills'){
+            return {ok:true,json: async () => (skillsMock)}
+        }else if(url === '/api/admin/histories'){
+            return {ok:true,json: async () => (answer_request_groupsMock)}
+        }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=&skillId='){
+            return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[2],answer_request_groupsMock[3]])}
+        }else if(url === '/api/admin/histories/select?answerDate=2023-08-01&departmentId=&skillId='){
+            return {ok:true,json: async () => ([answer_request_groupsMock[4]])}
+        }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=2&skillId='){
+            return {ok:true,json: async () => ([answer_request_groupsMock[3]])}
+        }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=&skillId=1,2'){
+            return {ok:true,json: async () => (answer_request_groupsMock)}
+        }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=&skillId=1'){
+        return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1]])}
+        }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=1&skillId='){
+            return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[2]])}
+        }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=&skillId=1'){
+            return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[3]])}
+        }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=1&skillId=1'){
+            return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[4]])}
+        }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=1&skillId=1'){
+            return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1]])}
+        }
+    })
     describe('絞り込みテスト',()=>{
         beforeEach(async () => {
-            global.fetch = jest.fn().mockImplementation((url,config)=>{
-                if(url === '/api/admin/histories/departments'){
-                    return {ok:true,json: async () => (departmentsMock)}
-                }else if(url === '/api/admin/histories/skills'){
-                    return {ok:true,json: async () => (skillsMock)}
-                }else if(url === '/api/admin/histories'){
-                    return {ok:true,json: async () => (answer_request_groupsMock)}
-                }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=&skillId='){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[2],answer_request_groupsMock[3]])}
-                }else if(url === '/api/admin/histories/select?answerDate=2023-08-01&departmentId=&skillId='){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[4]])}
-                }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=2&skillId='){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[3]])}
-                }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=&skillId=1,2'){
-                    return {ok:true,json: async () => (answer_request_groupsMock)}
-                }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=&skillId=1'){
-                return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1]])}
-                }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=1&skillId='){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[2]])}
-                }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=&skillId=1'){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[3]])}
-                }else if(url === '/api/admin/histories/select?answerDate=-01&departmentId=1&skillId=1'){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1],answer_request_groupsMock[4]])}
-                }else if(url === '/api/admin/histories/select?answerDate=2023-07-01&departmentId=1&skillId=1'){
-                    return {ok:true,json: async () => ([answer_request_groupsMock[0],answer_request_groupsMock[1]])}
-                }
-            })
             await act(()=>render(
                 <SelectHistoryProvider>
                     <AppRouterContextProviderMock router={{ push }}>
@@ -99,7 +99,6 @@ import { answer_request_groupsMock, departmentsMock, skillsMock } from './histor
         })
         describe('絞り込みapiリクエストのテスト',()=>{
             it('回答月の選択',async()=>{
-                const form = screen.getByRole('button',{name:"絞り込み"})
                 await user.selectOptions(selectMonth,'2023-07')
                 await user.click(form)
                 await waitFor (()=>{
